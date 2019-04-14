4ff7a1ae1f894c4c8c2f4f1c788b15b3bd2bf52c
GeorgeGUS <georgegus1@gmail.com>
2019-04-14 23:07:53 +0300

    Add Spinner in RandomPlanet on loading data

--- src/Components/RandomPlanet/RandomPlanet.js
+++ src/Components/RandomPlanet/RandomPlanet.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import SwapiService from '../../Services/swapi-service';
 
 import './RandomPlanet.css';
+import Spinner from '../Spinner/Spinner';
 
 class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   }
 
   constructor(props) {
@@ -17,7 +19,7 @@ class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false });
   }
 
   updatePlanet() {
@@ -28,43 +30,51 @@ class RandomPlanet extends Component {
   }
 
   render() {
-    const { id, name, population,
-      rotationPeriod, diameter } = this.state.planet;
-
+    const { planet, loading } = this.state;
     return (
-      <div className="random-planet jumbotron rounded">
-        <div className="row">
-          <div className="col-md-3">
-            <img className="image"
-              alt={name}
-              width="400"
-              height="400"
-              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-          </div>
-          <div className="col mt-md-0 mt-3">
-            <h2 className="subtitle">{name}</h2>
-            <table className="table">
-              <tbody>
-                <tr>
-                  <td>Population</td>
-                  <td>{population}</td>
-                </tr>
-                <tr>
-                  <td>Rotation Period</td>
-                  <td>{rotationPeriod}</td>
-                </tr>
-                <tr>
-                  <td>Diameter</td>
-                  <td>{diameter}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-
+      <div className="random-planet jumbotron rounded d-flex">
+        {
+          loading ?
+            <Spinner />
+            :
+            <PlanetView {...planet} />
+        }
       </div>
     );
   }
+}
+
+const PlanetView = ({ id, name, population, rotationPeriod, diameter }) => {
+  return (
+    <div className="row">
+      <div className="col-md-3">
+        <img className="image"
+          alt={name}
+          width="400"
+          height="400"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      </div>
+      <div className="col mt-md-0 mt-3">
+        <h2 className="subtitle">{name}</h2>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>Population</td>
+              <td>{population}</td>
+            </tr>
+            <tr>
+              <td>Rotation Period</td>
+              <td>{rotationPeriod}</td>
+            </tr>
+            <tr>
+              <td>Diameter</td>
+              <td>{diameter}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default RandomPlanet;
