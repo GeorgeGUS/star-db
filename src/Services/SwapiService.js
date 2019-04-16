5716102acafb33888e727829658e1a3bc940e013
GeorgeGUS <georgegus1@gmail.com>
2019-04-17 02:04:48 +0300

    Add getImageUrl fns

--- src/Services/SwapiService.js
+++ src/Services/SwapiService.js
@@ -1,5 +1,6 @@
 export default class SwapiService {
   _apiBase = 'https://swapi.co/api';
+  _imageBase = 'https://starwars-visualguide.com/assets/img';
 
   getResource = async url => {
     const res = await fetch(`${this._apiBase}${url}`);
@@ -41,6 +42,16 @@ export default class SwapiService {
   getStarship = async id => {
     const starship = await this.getResource(`/starships/${id}/`);
     return this._transformStarship(starship, id);
+  };
+
+  getPersonImage = ({ id }) => {
+    return `${this._imageBase}/characters/${id}.jpg`;
+  };
+  getPlanetImage = ({ id }) => {
+    return `${this._imageBase}/planets/${id}.jpg`;
+  };
+  getStarshipImage = ({ id }) => {
+    return `${this._imageBase}/starships/${id}.jpg`;
   };
 
   _transformPlanet = (planet, id) => {
