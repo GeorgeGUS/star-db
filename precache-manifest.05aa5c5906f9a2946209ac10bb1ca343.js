1379067112f777dcd990a9976c2503a343feb586
GeorgeGUS <georgegus1@gmail.com>
2019-04-19 15:10:33 +0300

    Updates

--- precache-manifest.05aa5c5906f9a2946209ac10bb1ca343.js
+++ precache-manifest.05aa5c5906f9a2946209ac10bb1ca343.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "a4374cee1119e23b4d85",
+    "revision": "fc4f6170ddfcbe105ba0",
     "url": "/star-db/static/css/main.1848ce37.chunk.css"
   },
   {
-    "revision": "a4374cee1119e23b4d85",
-    "url": "/star-db/static/js/main.67d8d69a.chunk.js"
+    "revision": "fc4f6170ddfcbe105ba0",
+    "url": "/star-db/static/js/main.40fa1e8b.chunk.js"
   },
   {
     "revision": "7fd0c1ce602d36977d9a",
     "url": "/star-db/static/js/runtime~main.82ef0156.js"
   },
   {
-    "revision": "1635f33a682f841dcf8b",
-    "url": "/star-db/static/js/2.74933640.chunk.js"
+    "revision": "22b3031411c227ee5898",
+    "url": "/star-db/static/js/2.c3308d9e.chunk.js"
   },
   {
     "revision": "e7f99c7391a7eb44ff7220ecab06ffb6",
     "url": "/star-db/static/media/death-star-explosion.e7f99c73.jpg"
   },
   {
-    "revision": "e634c6e72fcbf33681f15aa440da7f38",
+    "revision": "08073f83c752d1103ac80172dbe8a32a",
     "url": "/star-db/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/star-db/precache-manifest.8dfafc7959598c51c4f83367cf6bb423.js"
+  "/star-db/precache-manifest.05aa5c5906f9a2946209ac10bb1ca343.js"
 );
 
 workbox.clientsClaim();
